bb4a648931f42f1c8e2e49469d6d630b7debb3e6
George Chokhonelidze <g_chokhonelidze@outlook.com>
2022-10-07 15:01:22 -0500

    fix

---
+++ backend/index.js
@@ -0,0 +1,112 @@
+//const { ApolloServer, gql } = require("apollo-server");
+//const { restaurant } = require("./models/restaurant");
+//const { indexes } = require("./models/indexes");
+//const {dishes} = require("./models/dishes");
+//const mongoose = require("mongoose");
+import {ApolloServer,gql} from "apollo-server";
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault
+} from "apollo-server-core";
+import mongoose from "mongoose";
+import * as Pokemons from "./schemas/Pokemons.js"
+import * as PokemonsResolver from "./resolvers/Pokemons.js";
+import {} from 'dotenv/config';
+import  jwt from "jsonwebtoken";
+
+const DB = process.env.DB
+  ? process.env.DB
+  : "mongodb://mongo:27017/app_development";
+
+  const PORT = process.env.PORT || 4000;
+
+
+const types = [];
+const queries = [];
+const mutations = [];
+let resolverQueries = [];
+let resolverMutations = [];
+let objects = [];
+
+const schemas = [
+Pokemons,
+];
+const resolver = [
+  PokemonsResolver,
+];
+
+
+schemas.forEach((item)=>{
+  types.push(item.types);
+  queries.push(item.queries);
+  mutations.push(item.mutations);
+
+});
+
+resolver.forEach((item)=>{
+  resolverQueries = {...resolverQueries,...item.query};
+  resolverMutations = {...resolverMutations,...item.mutation};
+  if(item?.objects){
+    objects  = {...objects,...item?.objects};
+  }
+});
+const typeDefs = gql`
+  ${types.join('\n')}
+  type Query {
+    ${queries.join('\n')}
+  }
+  type Mutation {
+    ${mutations.join('\n')}
+  }
+`;
+
+
+const resolvers = {
+  Query: 
+    resolverQueries
+  ,
+  Mutation: resolverMutations,
+  ...objects,
+};
+console.log(resolvers);
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: "bounded",
+  context:  async ({ req }) => {
+    const authHeader = req.headers.authorization || '';
+    const accessTokenSecret = 'somerandomaccesstoken';
+    if(authHeader) {
+      const sp = authHeader.split(' ');
+      const loginUser= sp[0];
+      const token = sp[1]; 
+     return await jwt.verify(token, accessTokenSecret, (err, user) => {
+        if (err) {
+            return null;
+        }
+        return {user};
+    });
+    }
+
+  },
+  plugins: [
+    // Install a landing page plugin based on NODE_ENV
+    process.env.NODE_ENV === "production"
+      ? ApolloServerPluginLandingPageProductionDefault({
+          graphRef: "My-Graph-zz9m3@current",
+          footer: false,
+        })
+      : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+  ],
+});
+
+// The `listen` method launches a web server.
+mongoose.connect(DB, { useNewUrlParser: true }).then(() => {
+  server.listen(PORT).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+});
---
+++ backend/resolvers/Pokemons.js
@@ -0,0 +1,87 @@
+
+import pokemonsData from '../src/pokemons.json' assert { type: 'json' };
+//import { indexes } from "../models/indexes.json";
+let favorites = new Map();
+
+export const query = {
+  pokemons: (_, args) => {
+    const { limit, offset, search, filter } = args.query;
+    let pokemons = pokemonsData;
+    if (search) {
+      const regex = new RegExp(search, "i");
+      pokemons = pokemons.filter((p) => {
+        return p.name.match(regex);
+      });
+    }
+    if (filter) {
+      if (filter.type) {
+        const regex = new RegExp(filter.type, "i");
+        pokemons = pokemons.filter((p) => {
+          return p.types.match(regex);
+        });
+      }
+      if (filter.isFavorite) {
+        pokemons = pokemons.filter((p) => {
+          return !!favorites.get(p.id);
+        });
+      }
+    }
+    const count = pokemons.length;
+    const edges = pokemons.slice(offset, offset + limit);
+
+    return {
+      limit,
+      offset,
+      count,
+      edges,
+    };
+  },
+  pokemonById: (_, args) =>
+    pokemonsData.find((pokemon) => pokemon.id === args.id),
+  pokemonByName: (_, args) =>
+    pokemonsData.find(
+      (pokemon) => pokemon.name.toLowerCase() === args.name.toLowerCase()
+    ),
+  pokemonTypes: (_, args) => {
+    let data = pokemonsData.flatMap((s) => s.types);
+    return data.filter((value, index, self) => {
+      return self.indexOf(value) === index;
+    });
+  },
+};
+export const mutation = {
+    favoritePokemon: (_, args) => {
+        const pokemon = pokemonsData.find((pokemon) => pokemon.id === args.id);
+        if (!pokemon) throw Error("Pokemon not found");
+        favorites.set(args.id, true);
+        return pokemon;
+      },
+      unFavoritePokemon: (_, args) => {
+        const pokemon = pokemonsData.find((pokemon) => pokemon.id === args.id);
+        if (!pokemon) throw Error("Pokemon not found");
+        favorites.set(args.id, false);
+        return pokemon;
+      },
+}
+
+export const objects = {
+    Pokemon: {
+        number: (pokemon) => parseInt(pokemon.id, 10),
+        image: (pokemon) =>
+          `https://img.pokemondb.net/artwork/${pokemon.name
+            .toLowerCase()
+            .replace(/[&\\/\\\\#,+()$~%.'":*?<>{}]/g, "")
+            .replace(" ", "-")}.jpg`,
+        sound: (pokemon) => `${BASE_URL}/sounds/${parseInt(pokemon.id, 10)}`,
+        evolutions: (pokemon) =>
+          _.map(pokemon.evolutions || [], (ev) => ({
+            ...ev,
+            id: _.padStart(ev.id, 3, "0"),
+          })),
+        isFavorite: (pokemon) => !!favorites.get(pokemon.id),
+      },
+      PokemonAttack: {
+        fast: (pokemonAttack) => pokemonAttack.fast || [],
+        special: (pokemonAttack) => pokemonAttack.special || [],
+      },
+}
