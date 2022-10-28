70446b27dc0496d1744b6b1e417eea9c5314e9c7
George Chokhonelidze <g_chokhonelidze@outlook.com>
2022-10-28 09:42:49 -0500

    favorites was pulling just 10 changed to 1000

--- react/pokedex/src/pages/favorites.js
+++ react/pokedex/src/pages/favorites.js
@@ -1,26 +1,32 @@
 import React from "react";
-import { View,Filter,Search } from "../App";
+import { View, Filter, Search } from "../App";
 import { NormalView, ListView } from "../components/viewGenerator";
 import Loading from "../components/loading";
 import { query } from "../functions/queries";
 
 /**
- * 
+ *
  * @description selects favorite pokemons from the database
  * @returns {React.NormalView,React.ListView} NormalView and ListView
  */
 function Favorites() {
-    const [isLoading,setLoading] = React.useState(true);
-    const [data,setData] = React.useState(null);
-    const [style, setStyle] = React.useContext(View);
-    const [isUpdated,setUpdated] = React.useState(false);
-    const [filter,setFilter] = React.useContext(Filter);
-    const [search,setSearch] = React.useContext(Search);
-    /**
-     * loads favorite data from database.
-     */
-    const q = `query getPokemons($isFavorite:Boolean!,$type : String, $search: String) {
-      pokemons(query:{filter:{isFavorite:$isFavorite,type:$type},search:$search}) {
+  const [isLoading, setLoading] = React.useState(true);
+  const [data, setData] = React.useState(null);
+  const [style, setStyle] = React.useContext(View);
+  const [isUpdated, setUpdated] = React.useState(false);
+  const [filter, setFilter] = React.useContext(Filter);
+  const [search, setSearch] = React.useContext(Search);
+  /**
+   * loads favorite data from database.
+   * @param limit: Integer
+   * @param offset: Integer
+   * @param type: String
+   * @param search: String
+   * @param isFavorite:Boolean
+   * @returns {data} pokemons
+   */
+  const q = `query getPokemons($limit : Int, $offset : Int,$isFavorite:Boolean!,$type : String, $search: String) {
+      pokemons(query:{limit:$limit,offset:$offset,filter:{isFavorite:$isFavorite,type:$type},search:$search}) {
           edges {
               name,
               types,
@@ -31,30 +37,33 @@ function Favorites() {
       }
   }
   `;
-   
-    const update=React.useCallback(async () => {
-      let params = {
-        isFavorite:true,
-        type:filter,
-        search:search
-      }
-      setData(null);
-      query(q,params,setData);
-      setLoading(false);
-    },[q,search,filter]);
-    React.useEffect(()=>{
-      update();
-    },[filter,search])
+   /**
+    * pulls first 1000 records on update
+    */
+  const update = React.useCallback(async () => {
+    let params = {
+      isFavorite: true,
+      type: filter,
+      search: search,
+      limit: 1000,
+      offset: 0
+    };
+    setData(null);
+    query(q, params, setData);
+    setLoading(false);
+  }, [q, search, filter]);
+  React.useEffect(() => {
+    update();
+  }, [filter, search]);
   let view = null;
-    /**
+  /**
    * variable style changes the layout of the page.
    * if style is 0 it will load greed layout.
    * if style is 1 it will load list layout.
    */
-  if(isLoading){
-      view = <Loading />
-  }
-  else if (style === 0) {
+  if (isLoading) {
+    view = <Loading />;
+  } else if (style === 0) {
     if (data) {
       view = <NormalView data={data} />;
     }
@@ -62,6 +71,5 @@ function Favorites() {
     view = <ListView data={data} />;
   }
   return <>{view}</>;
-
 }
 export default Favorites;
